b13ee9dd73acbba4edfe33eda155df7986b9acd6
최영준 <jjmullan24@gmail.com>
2025-11-20 16:10:31 +0900

    feature: comment 패칭 및 서버 상태 관리 로직 추가

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -1,10 +1,26 @@
 import supabase from "@/lib/supabase";
 
 /**
+ * 특정 게시물에 대한 댓글을 조회하는 기능
+ * @param postId 댓글을 조회할 게시물의 ID
+ * @returns data : 댓글 정보
+ */
+export async function fetchComments(postId: number) {
+  const { data, error } = await supabase
+    .from("comment")
+    .select("*, author: profile!author_id (*)")
+    .eq("post_id", postId)
+    .order("created_at", { ascending: false });
+
+  if (error) throw error;
+  return data;
+}
+
+/**
  * 특정 게시물에 대한 댓글을 추가하는 기능
  * @param postId 댓글이 작성될 게시물의 ID
  * @param content 댓글의 상세 내용
- * @returns
+ * @returns data
  */
 export async function createComment({
   postId,

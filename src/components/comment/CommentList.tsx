9595c6881674a1bbe223f6a84585b8dbf35c3d47
최영준 <jjmullan24@gmail.com>
2025-11-20 16:16:13 +0900

    feature: comment 조회 기능 구현(~10.3)

--- src/components/comment/CommentList.tsx
+++ src/components/comment/CommentList.tsx
@@ -1,11 +1,23 @@
 import CommentItem from "@/components/comment/CommentItem";
+import Fallback from "@/components/Fallback";
+import Loader from "@/components/Loader";
+import { useCommentsData } from "@/hooks/queries/useCommentsData";
 
-export default function CommentList() {
+export default function CommentList({ postId }: { postId: number }) {
+  const {
+    data: comments,
+    error: fetchCommentError,
+    isPending: isFetchCommentPending,
+  } = useCommentsData(postId);
+
+  if (fetchCommentError) return <Fallback />;
+  if (isFetchCommentPending) return <Loader />;
+
   return (
     <div className="flex flex-col gap-5">
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
+      {comments.map((comment) => (
+        <CommentItem key={comment.id} {...comment} />
+      ))}
     </div>
   );
 }
--- src/pages/PostDetailPage.tsx
+++ src/pages/PostDetailPage.tsx
@@ -14,7 +14,7 @@ function PostDetailPage() {
       <PostItem postId={Number(postId)} type="DETAIL" />
       <div className="text-xl font-bold">댓글</div>
       <CommentEditor postId={Number(postId)} />
-      <CommentList />
+      <CommentList postId={Number(postId)} />
     </div>
   );
 }

f49646f943409ad878b23ea8d03ebd57bec2b61e
최영준 <jjmullan24@gmail.com>
2025-11-20 15:23:03 +0900

    feature: 댓글 UI 구현(~10.1)

--- src/pages/PostDetailPage.tsx
+++ src/pages/PostDetailPage.tsx
@@ -1,8 +1,21 @@
+import CommentEditor from "@/components/comment/CommentEditor";
+import CommentList from "@/components/comment/CommentList";
+import PostItem from "@/components/post/PostItem";
+import { Navigate, useParams } from "react-router";
+
 function PostDetailPage() {
+  const params = useParams();
+  const postId = params.postId;
+
+  if (!postId) return <Navigate to="/" />;
+
   return (
-    <>
-      <h1>PostDetailPage Component</h1>
-    </>
+    <div className="flex flex-col gap-y-5">
+      <PostItem postId={Number(postId)} type="DETAIL" />
+      <div className="text-xl font-bold">댓글</div>
+      <CommentEditor />
+      <CommentList />
+    </div>
   );
 }
 
